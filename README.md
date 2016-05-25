24def02ee1d847e4718a6188992f1f679892a568
wooklym <wooklym@gmail.com>
2016-05-25 18:04:26 +0900

    add lesson2

--- README.md
+++ README.md
@@ -17,3 +17,10 @@
 - 변수를 써보자
 - 주석이란?
 - 안드로이드 UI 둘러보기
+
+## [Lesson3](https://github.com/wooklym/study-android/tree/master/lessons3)
+
+- Layout XML에 있는 UI 컴포넌트를 Java 코드에서 불러오는 과정 자세히 살펴보기
+- 변수의 종류와 차이점 자세히 알아보기
+- 덧셈, 뺄셈, 곱셈, 나눗셈
+- 로그인 화면에 로봇방지를 위한 계산 문제 넣기
---
+++ lesson2/Login/app/src/main/java/com/techbridge/login/MainActivity.java
@@ -0,0 +1,31 @@
+package com.techbridge.login;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button asdfButton = (Button) findViewById(R.id.asdfButton);
+
+
+        Button login = (Button) findViewById(R.id.login);
+
+        Button.OnClickListener listener = new Button.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                System.out.println("click occured");
+            }
+        };
+
+        login.setOnClickListener(listener);
+    }
+}
